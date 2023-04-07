5f7a405a6745b5890e00ff346866309ff8a0f857
rdemko2332 <rdemko2332@gmail.com>
2023-04-07 15:29:01 -0400

    Fixing naming issue

--- bin/checkForUpdate.pl
+++ bin/checkForUpdate.pl
@@ -14,8 +14,8 @@ open(my $data, '<', $inputFile) || die "Could not open file $inputFile: $!";
 open(OUT,">$outputFile");
 while (my $line = <$data>) {
     chomp $line;
-    if ($line =~  /^Core/) { 
-        my ($isCore,$project,$proteomeName,$build,$abbrev) = split(/\,/, $line);
+    if ($line =~  /^eupath/) { 
+        my ($eupath,$project,$proteomeName,$build,$abbrev) = split(/\,/, $line);
         `wget "https://$project.org/common/downloads/Current_Release/$proteomeName/fasta/data/$project-${build}_${proteomeName}_AnnotatedProteins.fasta" --output-document ./$abbrev.fasta --tries=5`;
 	if (-e "/storage/$abbrev.fasta") {
             my $needsUpdate = `diff /storage/$abbrev.fasta ./$abbrev.fasta -q`;
@@ -30,7 +30,7 @@ while (my $line = <$data>) {
         }
     }
     else {
-        my ($isCore,$proteomeId,$abbrev) = split(/\,/, $line);
+        my ($uniprot,$proteomeId,$abbrev) = split(/\,/, $line);
         `wget "https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&query=%28%28proteome%3A${proteomeId}%29%29"--tries=5 --output-document ./$abbrev.fasta.gz`;
 	if (-e "/storage/$abbrev.fasta.gz") {
             my $needsUpdate = `diff /storage/$abbrev.fasta ./$abbrev.fasta -q`;	    
