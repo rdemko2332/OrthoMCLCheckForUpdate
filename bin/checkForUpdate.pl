e0f3c93363f77ffebfce22e88e0c23dc0618f0a6
rdemko2332 <rdemko2332@gmail.com>
2023-04-25 12:09:14 -0400

    Updating determination of difference

--- bin/checkForUpdate.pl
+++ bin/checkForUpdate.pl
@@ -12,35 +12,29 @@ my ($inputFile, $storageDir, $outputFile);
 
 open(my $data, '<', $inputFile) || die "Could not open file $inputFile: $!";
 open(OUT,">$outputFile");
+
 while (my $line = <$data>) {
     chomp $line;
-    if ($line =~  /^eupath/) { 
-        my ($eupath,$project,$proteomeName,$build,$abbrev) = split(/\,/, $line);
-        `wget "https://$project.org/common/downloads/Current_Release/$proteomeName/fasta/data/$project-${build}_${proteomeName}_AnnotatedProteins.fasta" --output-document ./$abbrev.fasta --tries=5`;
-	if (-e "/storage/$abbrev.fasta") {
-            my $needsUpdate = `diff /storage/$abbrev.fasta ./$abbrev.fasta -q`;
-            if (length($needsUpdate) != 0) {
-	        `cp $abbrev.fasta /storage`;
+
+    if ($line =~  /^Uniprot/) { 
+        my ($downloadType,$proteomeId,$abbrev) = split(/\,/, $line);
+	`wget "https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&query=proteome%3A${proteomeId}" --tries=5 --output-document ./$abbrev.fasta.gz`;
+
+        `gunzip $abbrev.fasta.gz`;
+	`grep ">" $abbrev.fasta > newHeaders.txt`;
+	`sort newHeaders.txt -o sortedHeaders.txt`;
+	
+	if (-e "/storage/$abbrev.txt") {
+            my $needsUpdate = `diff /storage/$abbrev.txt ./sortedHeaders.txt -q`;
+
+	    if (length($needsUpdate) != 0) {
+	        `cp sortedHeaders.txt /storage/$abbrev.txt`;
 	        print OUT "$abbrev\n";
             }
-	}
-	else {
-	    `cp $abbrev.fasta /storage/`;
-	    print OUT "$abbrev\n";
         }
-    }
-    else {
-        my ($uniprot,$proteomeId,$abbrev) = split(/\,/, $line);
-        `wget "https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&query=%28%28proteome%3A${proteomeId}%29%29"--tries=5 --output-document ./$abbrev.fasta.gz`;
-	if (-e "/storage/$abbrev.fasta.gz") {
-            my $needsUpdate = `diff /storage/$abbrev.fasta ./$abbrev.fasta -q`;	    
-            if (length($needsUpdate) != 0) {
-	        `cp $abbrev.fasta /storage`;
-	        print OUT "$abbrev\n";
-            }
-	}
+
 	else {
-	    `cp $abbrev.fasta.gz /storage/`;
+	    `cp sortedHeaders.txt /storage/$abbrev.txt`;
 	    print OUT "$abbrev\n";
         }
     }
